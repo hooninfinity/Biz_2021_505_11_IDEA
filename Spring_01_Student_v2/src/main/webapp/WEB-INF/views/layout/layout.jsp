ca5946f0f74cae3b7f94277d0ed29e00142c00cf
hooninfinity <hooninfinity@naver.com>
2021-11-08 17:32:50 +0900

    tiles 를 이용한 Student 프로젝트

---
+++ Spring_01_Student_v2/src/main/webapp/WEB-INF/views/layout/layout.jsp
@@ -0,0 +1,14 @@
+<%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Title</title>
+</head>
+<body>
+    <tiles:insertAttribute name="header"/>
+    <tiles:insertAttribute name="nav"/>
+    <tiles:insertAttribute name="content"/>
+</body>
+</html>
---
+++ Spring_01_Student_v2_A/src/main/java/com/callor/student/controller/StudentController.java
@@ -0,0 +1,37 @@
+package com.callor.student.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/st")
+public class StudentController {
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list() {
+        // tiles_layout.xml 에 name 이 ST/* 로 설정된 곳을 
+        // 참조하여 view 를 rendering 하라
+        // name="ST/*" * 표대신 st_list 문자열이 배치
+        return "ST/st_list";
+    }
+    @RequestMapping(value = "/detail", method = RequestMethod.GET)
+    public String detail() {
+        return "ST/st_detail";
+    }
+    @RequestMapping(value = "/write", method = RequestMethod.GET)
+    public String write() {
+        return "ST/st_write";
+    }
+
+
+
+    @RequestMapping(value = "/list2", method = RequestMethod.GET)
+    public String list2() {
+        // tiles_layout.xml 에 name 이 ST/* 로 설정된 곳을
+        // 참조하여 view 를 rendering 하라
+        // name="ST/*" * 표대신 st_list 문자열이 배치
+        return "student/st_list";
+    }
+
+}
